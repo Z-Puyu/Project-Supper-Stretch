78bdd7e8daa70bf9a0123cc09cc64ec0afb37931
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2026-03-08 00:18:29 +0800

    Add AnimationResource

---
+++ Assets/GameplayAbilities/Runtime/Abilities/Resource/AnimationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameplayAbilities.Abilities.Resource {
+    [Serializable]
+    public record struct AnimationResource : IAbilityResource {
+        [field: SerializeField] public AnimationClip Clip { get; private set; }
+        
+        public static implicit operator AnimationClip(AnimationResource resource) => resource.Clip;
+    }
+}
--- Assets/SOULS/Runtime/GameplayAbilities/Executions/DodgeAbilityExecution.cs
+++ Assets/SOULS/Runtime/GameplayAbilities/Executions/DodgeAbilityExecution.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Threading;
 using GameplayAbilities.Abilities;
+using GameplayAbilities.Abilities.Resource;
 using GameplayAbilities.Common;
 using UnityEngine;
 
 namespace SOULS.GameplayAbilities.Executions {
     [Serializable]
     public sealed class DodgeAbilityExecution : AbilityExecution {
-        [field: SerializeField] private AnimationClip? DodgeAnimation { get; set; }
-        [field: SerializeField] private AbilityResourceKey<string> Resource1 { get; set; }
-        [field: SerializeField] private AbilityResourceKey<string> Resource2 { get; set; }
+        [field: SerializeField] private AnimationResource DodgeAnimation { get; set; }
         
         protected override Awaitable Execute(
             AbilitySystemController source, IUserData? userData, CancellationToken interrupt
